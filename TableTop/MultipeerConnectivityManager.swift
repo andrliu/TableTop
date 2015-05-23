f360b3eaf5ddda9fc7b40b6c0cf39485eb4c2791
andrliu <dieseliu321@gmail.com>
2015-05-23 15:59:14 -0700

    first commit

---
+++ TableTop/MultipeerConnectivityManager.swift
@@ -0,0 +1,13 @@
+//
+//  MultipeerConnectivityManager.swift
+//  TableTop
+//
+//  Created by Andrew Liu on 5/20/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+import Cocoa
+
+class MultipeerConnectivityManager: NSObject {
+
+}
