71f79e093a88059b4a60e82807f1e46f9869abda
andrliu <dieseliu321@gmail.com>
2015-05-24 00:57:59 -0700

    MCManager init

--- TableTop/MultipeerConnectivityManager.swift
+++ TableTop/MultipeerConnectivityManager.swift
@@ -6,8 +6,77 @@
 //  Copyright (c) 2015 Andrew Liu. All rights reserved.
 //
 
-import Cocoa
+import Foundation
+import MultipeerConnectivity
 
-class MultipeerConnectivityManager: NSObject {
+class MultipeerConnectivityManager: NSObject, MCSessionDelegate, MCNearbyServiceBrowserDelegate, MCNearbyServiceAdvertiserDelegate
+ {
+    var serviceType: String = "Game1"
+    var session: MCSession!
+    var peer: MCPeerID!
+    var browser: MCNearbyServiceBrowser!
+    var advertiser: MCNearbyServiceAdvertiser!
+    
+    override init() {
+        super.init()
 
+        peer = MCPeerID(displayName: UIDevice.currentDevice().name)
+        
+        self.session = MCSession(peer: peer)
+        self.session.delegate = self
+        
+        self.browser = MCNearbyServiceBrowser(peer: peer, serviceType: serviceType)
+        self.browser.delegate = self
+
+        self.advertiser = MCNearbyServiceAdvertiser(peer: peer, discoveryInfo: nil, serviceType: serviceType)
+        self.advertiser.delegate = self
+    }
+    
 }
+
+extension MultipeerConnectivityManager: MCSessionDelegate {
+    // Remote peer changed state
+    func session(session: MCSession!, peer peerID: MCPeerID!, didChangeState state: MCSessionState) {
+        
+    }
+    
+    // Received data from remote peer
+    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    // Received a byte stream from remote peer
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    // Start receiving a resource from remote peer
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+        
+    }
+    
+    // Finished receiving a resource from remote peer and saved the content in a temporary location - the app is responsible for moving the file to a permanent location within its sandbox
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+        
+    }
+}
+
+extension MultipeerConnectivityManager: MCNearbyServiceBrowserDelegate {
+    // Found a nearby advertising peer
+    func browser(browser: MCNearbyServiceBrowser!, foundPeer peerID: MCPeerID!, withDiscoveryInfo info: [NSObject : AnyObject]!) {
+    
+    }
+
+    // A nearby peer has stopped advertising
+    func browser(browser: MCNearbyServiceBrowser!, lostPeer peerID: MCPeerID!) {
+    
+    }
+}
+
+extension MultipeerConnectivityManager: MCNearbyServiceAdvertiserDelegate {
+    // Incoming invitation request.  Call the invitationHandler block with YES and a valid session to connect the inviting peer to the session.
+    func advertiser(advertiser: MCNearbyServiceAdvertiser!, didReceiveInvitationFromPeer peerID: MCPeerID!, withContext context: NSData!, invitationHandler: ((Bool, MCSession!) -> Void)!) {
+    
+    }
+}
+
